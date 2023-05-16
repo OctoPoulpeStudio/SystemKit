2d72738c13e062cf865b7fc7a7593853032b45a8
Ewen Riallan <121026779+EwenRiallanOcto@users.noreply.github.com>
2023-05-16 18:03:21 +0200

    the package should now be available for iOS and macOS

--- Example/main.swift
+++ Example/main.swift
@@ -30,35 +30,22 @@ import SystemKit
 print("// MACHINE STATUS")
 
 print("\n-- CPU --")
-print("\tPHYSICAL CORES:  \(System.physicalCores())")
-print("\tLOGICAL CORES:   \(System.logicalCores())")
+print("\tPHYSICAL CORES: \(System.physicalCores())")
+print("\tLOGICAL CORES: \(System.logicalCores())")
 
 var sys = System()
 let cpuUsage = sys.usageCPU()
-print("\tSYSTEM:          \(Int(cpuUsage.system))%")
-print("\tUSER:            \(Int(cpuUsage.user))%")
-print("\tIDLE:            \(Int(cpuUsage.idle))%")
-print("\tNICE:            \(Int(cpuUsage.nice))%")
+print("\tSYSTEM: \(Int(cpuUsage.system))%")
+print("\tUSER: \(Int(cpuUsage.user))%")
+print("\tIDLE: \(Int(cpuUsage.idle))%")
+print("\tNICE: \(Int(cpuUsage.nice))%")
 
 
 print("\n-- MEMORY --")
-print("\tPHYSICAL SIZE:   \(System.physicalMemory())GB")
-
-let memoryUsage = System.memoryUsage()
-func memoryUnit(_ value: Double) -> String {
-    if value < 1.0 { return String(Int(value * 1000.0)) + "MB" }
-    else           { return String(format:"%.2f", value) + "GB" }
-}
-
-print("\tFREE:            \(memoryUnit(memoryUsage.free))")
-print("\tWIRED:           \(memoryUnit(memoryUsage.wired))")
-print("\tACTIVE:          \(memoryUnit(memoryUsage.active))")
-print("\tINACTIVE:        \(memoryUnit(memoryUsage.inactive))")
-print("\tCOMPRESSED:      \(memoryUnit(memoryUsage.compressed))")
-
+print("\tPHYSICAL SIZE: \(System.physicalMemory())GB")
 
 print("\n-- SYSTEM --")
-print("\tMODEL:           \(System.modelName())")
+print("\tMODEL: \(System.modelName())")
 
 //let names = System.uname()
 //print("\tSYSNAME:         \(names.sysname)")
@@ -68,59 +55,26 @@ print("\tMODEL:           \(System.modelName())")
 //print("\tMACHINE:         \(names.machine)")
 
 let uptime = System.uptime()
-print("\tUPTIME:          \(uptime.days)d \(uptime.hrs)h \(uptime.mins)m " +
-                            "\(uptime.secs)s")
-
-let counts = System.processCounts()
-print("\tPROCESSES:       \(counts.processCount)")
-print("\tTHREADS:         \(counts.threadCount)")
+print("\tUPTIME: \(uptime.days)d \(uptime.hrs)h \(uptime.mins)m " + "\(uptime.secs)s")
 
 let loadAverage = System.loadAverage().map { String(format:"%.2f", $0) }
-print("\tLOAD AVERAGE:    \(loadAverage)")
-print("\tMACH FACTOR:     \(System.machFactor())")
+print("\tLOAD AVERAGE: \(loadAverage)")
+print("\tMACH FACTOR: \(System.machFactor())")
 
-
-print("\n-- POWER --")
-let cpuThermalStatus = System.CPUPowerLimit()
-
-print("\tCPU SPEED LIMIT: \(cpuThermalStatus.processorSpeed)%")
-print("\tCPUs AVAILABLE:  \(cpuThermalStatus.processorCount)")
-print("\tSCHEDULER LIMIT: \(cpuThermalStatus.schedulerTime)%")
-
-print("\tTHERMAL LEVEL:   \(System.thermalLevel().rawValue)")
-
-var battery = Battery()
-if battery.open() != kIOReturnSuccess { exit(0) }
-
-print("\n-- BATTERY --")
-print("\tAC POWERED:      \(battery.isACPowered())")
-print("\tCHARGED:         \(battery.isCharged())")
-print("\tCHARGING:        \(battery.isCharging())")
-print("\tCHARGE:          \(battery.charge())%")
-print("\tCAPACITY:        \(battery.currentCapacity()) mAh")
-print("\tMAX CAPACITY:    \(battery.maxCapactiy()) mAh")
-print("\tDESGIN CAPACITY: \(battery.designCapacity()) mAh")
-print("\tCYCLES:          \(battery.cycleCount())")
-print("\tMAX CYCLES:      \(battery.designCycleCount())")
-print("\tTEMPERATURE:     \(battery.temperature())°C")
-print("\tTIME REMAINING:  \(battery.timeRemainingFormatted())")
-
-_ = battery.close()
 
 print("\n// SMC")
 
 let maxTemperatureCelsius = 128.0
 
 enum ANSIColor: String {
-    case Off    = "\u{001B}[0;0m"
-    case Red    = "\u{001B}[0;31m"
-    case Green  = "\u{001B}[0;32m"
+    case Off = "\u{001B}[0;0m"
+    case Red = "\u{001B}[0;31m"
+    case Green = "\u{001B}[0;32m"
     case Yellow = "\u{001B}[0;33m"
-    case Blue   = "\u{001B}[0;34m"
+    case Blue = "\u{001B}[0;34m"
 }
 
-func warningLevel(value: Double, maxValue: Double) -> (name: String,
-                                                       color: ANSIColor) {
+func warningLevel(value: Double, maxValue: Double) -> (name: String, color: ANSIColor) {
     let percentage = value / maxValue
 
     switch percentage {
@@ -131,169 +85,3 @@ func warningLevel(value: Double, maxValue: Double) -> (name: String,
     default:                   return ("Crisis", ANSIColor.Red)
     }
 }
-
-func printTemperatureInformation(known: Bool = true) {
-    print("\n-- Temperature --")
-
-    let sensors: [TemperatureSensor]
-    do {
-        if known {
-            sensors = try SMCKit.allKnownTemperatureSensors().sorted
-                                                           { $0.name < $1.name }
-        } else {
-            sensors = try SMCKit.allUnknownTemperatureSensors()
-        }
-
-    } catch {
-        print(error)
-        return
-    }
-
-
-    let sensorWithLongestName = sensors.max { $0.name.count <
-                                                     $1.name.count }
-
-    guard let longestSensorNameCount = sensorWithLongestName?.name.count else {
-        print("No temperature sensors found")
-        return
-    }
-
-
-    for sensor in sensors {
-        let padding = String(repeating: " ",
-                             count: longestSensorNameCount - sensor.name.count)
-
-        let smcKey  = "(\(sensor.code.toString()))"
-        print("\t\(sensor.name + padding)   \(smcKey)  ", terminator: "")
-
-
-        guard let temperature = try? SMCKit.temperature(sensor.code) else {
-            print("NA")
-            return
-        }
-
-        let warning = warningLevel(value: temperature, maxValue: maxTemperatureCelsius)
-        let level   = "(\(warning.name))"
-        let color   = warning.color
-
-        print("\(color.rawValue)\(temperature)°C \(level)" +
-              "\(ANSIColor.Off.rawValue)")
-    }
-}
-
-func printFanInformation() {
-    print("\n-- Fan --")
-
-    let allFans: [Fan]
-    do {
-        allFans = try SMCKit.allFans()
-    } catch {
-        print(error)
-        return
-    }
-
-    if allFans.count == 0 { print("No fans found") }
-
-    for fan in allFans {
-        print("[id \(fan.id)] \(fan.name)")
-        print("\tMin:      \(fan.minSpeed) RPM")
-        print("\tMax:      \(fan.maxSpeed) RPM")
-
-        guard let currentSpeed = try? SMCKit.fanCurrentSpeed(fan.id) else {
-            print("\tCurrent:  NA")
-            return
-        }
-
-        let warning = warningLevel(value: Double(currentSpeed),
-                                   maxValue: Double(fan.maxSpeed))
-        let level = "(\(warning.name))"
-        let color = warning.color
-        print("\tCurrent:  \(color.rawValue)\(currentSpeed) RPM \(level)" +
-                                                    "\(ANSIColor.Off.rawValue)")
-    }
-}
-
-func printPowerInformation() {
-    let information: BatteryInfo
-    do {
-        information = try SMCKit.batteryInformation()
-    } catch {
-        print(error)
-        return
-    }
-
-    print("\n-- Power --")
-    print("\tAC Present:       \(information.isACPresent)")
-    print("\tBattery Powered:  \(information.isBatteryPowered)")
-    print("\tCharging:         \(information.isCharging)")
-    print("\tBattery Ok:       \(information.isBatteryOk)")
-    print("\tBattery Count:    \(information.batteryCount)")
-}
-
-func printMiscInformation() {
-    print("\n-- Misc --")
-
-    let ODDStatus: Bool
-    do {
-        ODDStatus = try SMCKit.isOpticalDiskDriveFull()
-    } catch SMCKit.SMCError.keyNotFound { ODDStatus = false }
-      catch {
-        print(error)
-        return
-    }
-
-    print("\tDisc in ODD:      \(ODDStatus)")
-}
-
-func printAll() {
-    printTemperatureInformation()
-    printFanInformation()
-    printPowerInformation()
-    printMiscInformation()
-}
-
-func checkKey(key: String) {
-    if key.count != 4 {
-        print("Must be a FourCC (four-character code)")
-        return
-    }
-
-    do {
-        let isValid = try SMCKit.isKeyFound(FourCharCode(fromString: key))
-        let answer = isValid ? "valid" : "invalid"
-
-        print("\(key) is a \(answer) SMC key on this machine")
-    } catch { print(error) }
-}
-
-func setMinFanSpeed(fanId: Int, fanSpeed: Int) {
-    do {
-        let fan = try SMCKit.fan(fanId)
-        let currentSpeed = try SMCKit.fanCurrentSpeed(fanId)
-
-        try SMCKit.fanSetMinSpeed(fanId, speed: fanSpeed)
-
-        print("Min fan speed set successfully")
-        print("[id \(fan.id)] \(fan.name)")
-        print("\tMin (Previous):  \(fan.minSpeed) RPM")
-        print("\tMin (Target):    \(fanSpeed) RPM")
-        print("\tCurrent:         \(currentSpeed) RPM")
-    } catch SMCKit.SMCError.keyNotFound {
-        print("This machine has no fan with id \(fanId)")
-    } catch SMCKit.SMCError.notPrivileged {
-        print("This operation must be invoked as the superuser")
-    } catch SMCKit.SMCError.unsafeFanSpeed {
-        print("Invalid fan speed. Must be <= max fan speed")
-    } catch {
-        print(error)
-    }
-}
-
-do {
-    try SMCKit.open()
-    printAll()
-} catch {
-    print("Failed to open a connection to the SMC")
-    exit(EX_UNAVAILABLE)
-}
-SMCKit.close()
--- SystemKit/System.swift
+++ SystemKit/System.swift
@@ -25,7 +25,6 @@
 // THE SOFTWARE.
 
 import Darwin
-import IOKit.pwr_mgt
 import Foundation
 
 //------------------------------------------------------------------------------
@@ -35,18 +34,12 @@ import Foundation
 
 // As defined in <mach/tash_info.h>
 
-private let HOST_BASIC_INFO_COUNT         : mach_msg_type_number_t =
-                      UInt32(MemoryLayout<host_basic_info_data_t>.size / MemoryLayout<integer_t>.size)
-private let HOST_LOAD_INFO_COUNT          : mach_msg_type_number_t =
-                       UInt32(MemoryLayout<host_load_info_data_t>.size / MemoryLayout<integer_t>.size)
-private let HOST_CPU_LOAD_INFO_COUNT      : mach_msg_type_number_t =
-                   UInt32(MemoryLayout<host_cpu_load_info_data_t>.size / MemoryLayout<integer_t>.size)
-private let HOST_VM_INFO64_COUNT          : mach_msg_type_number_t =
-                      UInt32(MemoryLayout<vm_statistics64_data_t>.size / MemoryLayout<integer_t>.size)
-private let HOST_SCHED_INFO_COUNT         : mach_msg_type_number_t =
-                      UInt32(MemoryLayout<host_sched_info_data_t>.size / MemoryLayout<integer_t>.size)
-private let PROCESSOR_SET_LOAD_INFO_COUNT : mach_msg_type_number_t =
-              UInt32(MemoryLayout<processor_set_load_info_data_t>.size / MemoryLayout<natural_t>.size)
+private let HOST_BASIC_INFO_COUNT: mach_msg_type_number_t = UInt32(MemoryLayout<host_basic_info_data_t>.size / MemoryLayout<integer_t>.size)
+private let HOST_LOAD_INFO_COUNT: mach_msg_type_number_t = UInt32(MemoryLayout<host_load_info_data_t>.size / MemoryLayout<integer_t>.size)
+private let HOST_CPU_LOAD_INFO_COUNT: mach_msg_type_number_t = UInt32(MemoryLayout<host_cpu_load_info_data_t>.size / MemoryLayout<integer_t>.size)
+private let HOST_VM_INFO64_COUNT: mach_msg_type_number_t = UInt32(MemoryLayout<vm_statistics64_data_t>.size / MemoryLayout<integer_t>.size)
+private let HOST_SCHED_INFO_COUNT: mach_msg_type_number_t = UInt32(MemoryLayout<host_sched_info_data_t>.size / MemoryLayout<integer_t>.size)
+private let PROCESSOR_SET_LOAD_INFO_COUNT: mach_msg_type_number_t = UInt32(MemoryLayout<processor_set_load_info_data_t>.size / MemoryLayout<natural_t>.size)
 
 
 public struct System {
@@ -316,46 +309,11 @@ public struct System {
                 Double(result.2) / Double(LOAD_SCALE)]
     }
     
-
-    /// Total number of processes & threads
-    public static func processCounts() -> (processCount: Int, threadCount: Int) {
-        let data = System.processorLoadInfo()
-        return (Int(data.task_count), Int(data.thread_count))
-    }
-    
     
     /// Size of physical memory on this machine
     public static func physicalMemory(_ unit: Unit = .gigabyte) -> Double {
         return Double(System.hostBasicInfo().max_mem) / unit.rawValue
     }
-    
-    
-    /**
-    System memory usage (free, active, inactive, wired, compressed).
-    */
-    public static func memoryUsage() -> (free       : Double,
-                                         active     : Double,
-                                         inactive   : Double,
-                                         wired      : Double,
-                                         compressed : Double) {
-        let stats = System.VMStatistics64()
-        
-        let free     = Double(stats.free_count) * Double(PAGE_SIZE)
-                                                        / Unit.gigabyte.rawValue
-        let active   = Double(stats.active_count) * Double(PAGE_SIZE)
-                                                        / Unit.gigabyte.rawValue
-        let inactive = Double(stats.inactive_count) * Double(PAGE_SIZE)
-                                                        / Unit.gigabyte.rawValue
-        let wired    = Double(stats.wire_count) * Double(PAGE_SIZE)
-                                                        / Unit.gigabyte.rawValue
-        
-        // Result of the compression. This is what you see in Activity Monitor
-        let compressed = Double(stats.compressor_page_count) * Double(PAGE_SIZE)
-                                                        / Unit.gigabyte.rawValue
-        
-        return (free, active, inactive, wired, compressed)
-    }
-    
 
     /// How long has the system been up?
     public static func uptime() -> (days: Int, hrs: Int, mins: Int, secs: Int) {
@@ -399,113 +357,6 @@ public struct System {
 
         return (days, hrs, mins, secs)
     }
-
-
-    //--------------------------------------------------------------------------
-    // MARK: POWER
-    //--------------------------------------------------------------------------
-
-
-    /**
-    As seen via 'pmset -g therm' command.
-
-    Via <IOKit/pwr_mgt/IOPMLib.h>:
-
-        processorSpeed: Defines the speed & voltage limits placed on the CPU.
-                        Represented as a percentage (0-100) of maximum CPU
-                        speed.
-
-        processorCount: Reflects how many, if any, CPUs have been taken offline.
-                        Represented as an integer number of CPUs (0 - Max CPUs).
-
-                        NOTE: This doesn't sound quite correct, as pmset treats
-                              it as the number of CPUs available, NOT taken
-                              offline. The return value suggests the same.
-
-        schedulerTime:  Represents the percentage (0-100) of CPU time available.
-                        100% at normal operation. The OS may limit this time for
-                        a percentage less than 100%.
-    */
-    public static func CPUPowerLimit() -> (processorSpeed: Double,
-                                           processorCount: Int,
-                                           schedulerTime : Double) {
-        var processorSpeed = -1.0
-        var processorCount = -1
-        var schedulerTime  = -1.0
-
-        let status = UnsafeMutablePointer<Unmanaged<CFDictionary>?>.allocate(capacity: 1)
-
-        let result = IOPMCopyCPUPowerStatus(status)
-
-        #if DEBUG
-            // TODO: kIOReturnNotFound case as seen in pmset
-            if result != kIOReturnSuccess {
-                print("ERROR - \(#file):\(#function) - kern_result_t = "
-                        + "\(result)")
-            }
-        #endif
-
-
-        if result == kIOReturnSuccess,
-           let data = status.move()?.takeUnretainedValue() {
-                let dataMap = data as NSDictionary
-
-                // TODO: Force unwrapping here should be safe, as
-                //       IOPMCopyCPUPowerStatus() defines the keys, but the
-                //       the cast (from AnyObject) could be problematic
-                processorSpeed = dataMap[kIOPMCPUPowerLimitProcessorSpeedKey]!
-                                                                      as! Double
-                processorCount = dataMap[kIOPMCPUPowerLimitProcessorCountKey]!
-                                                                      as! Int
-                schedulerTime  = dataMap[kIOPMCPUPowerLimitSchedulerTimeKey]!
-                                                                      as! Double
-        }
-
-        status.deallocate()
-
-        return (processorSpeed, processorCount, schedulerTime)
-    }
-
-
-    /// Get the thermal level of the system. As seen via 'pmset -g therm'
-    public static func thermalLevel() -> System.ThermalLevel {
-        var thermalLevel: UInt32 = 0
-
-        let result = IOPMGetThermalWarningLevel(&thermalLevel)
-
-        if result == kIOReturnNotFound {
-            return System.ThermalLevel.NotPublished
-        }
-
-
-        #if DEBUG
-            if result != kIOReturnSuccess {
-                print("ERROR - \(#file):\(#function) - kern_result_t = "
-                        + "\(result)")
-            }
-        #endif
-
-
-        // TODO: Thermal warning level values no longer available through
-        //       IOKit.pwr_mgt module as of Xcode 6.3 Beta 3. Not sure if thats
-        //       intended behaviour or a bug, will investigate. For now
-        //       hardcoding values, will move all power related calls to a
-        //       separate struct.
-        switch thermalLevel {
-            case 0:
-                // kIOPMThermalWarningLevelNormal
-                return System.ThermalLevel.Normal
-            case 5:
-                // kIOPMThermalWarningLevelDanger
-                return System.ThermalLevel.Danger
-            case 10:
-                // kIOPMThermalWarningLevelCrisis
-                return System.ThermalLevel.Crisis
-            default:
-                return System.ThermalLevel.Unknown
-        }
-    }
-
 
     //--------------------------------------------------------------------------
     // MARK: PRIVATE METHODS
@@ -580,84 +431,6 @@ public struct System {
             }
         #endif
 
-        return data
-    }
-    
-    
-    fileprivate static func processorLoadInfo() -> processor_set_load_info {
-        // NOTE: Duplicate load average and mach factor here
-        
-        var pset   = processor_set_name_t()
-        var result = processor_set_default(machHost, &pset)
-        
-        if result != KERN_SUCCESS {
-            #if DEBUG
-                print("ERROR - \(#file):\(#function) - kern_result_t = "
-                        + "\(result)")
-            #endif
-
-            return processor_set_load_info()
-        }
-
-        
-        var count    = PROCESSOR_SET_LOAD_INFO_COUNT
-        let info_out = processor_set_load_info_t.allocate(capacity: 1)
-        
-        result = info_out.withMemoryRebound(to: integer_t.self, capacity: Int(count)) {
-            processor_set_statistics(pset,
-                                     PROCESSOR_SET_LOAD_INFO,
-                                     $0,
-                                     &count)
-        }
-
-        #if DEBUG
-            if result != KERN_SUCCESS {
-                print("ERROR - \(#file):\(#function) - kern_result_t = "
-                        + "\(result)")
-            }
-        #endif
-
-
-        // This is isn't mandatory as I understand it, just helps keep the ref
-        // count correct. This is because the port is to the default processor
-        // set which should exist by default as long as the machine is running
-        mach_port_deallocate(mach_task_self_, pset)
-
-        let data = info_out.move()
-        info_out.deallocate()
-        
-        return data
-    }
-    
-    
-    /**
-    64-bit virtual memory statistics. This should apply to all Mac's that run
-    10.9 and above. For iOS, iPhone 5S, iPad Air & iPad Mini 2 and on.
-    
-    Swift runs on 10.9 and above, and 10.9 is x86_64 only. On iOS though its 7
-    and above, with both ARM & ARM64.
-    */
-    fileprivate static func VMStatistics64() -> vm_statistics64 {
-        var size     = HOST_VM_INFO64_COUNT
-        let hostInfo = vm_statistics64_t.allocate(capacity: 1)
-        
-        let result = hostInfo.withMemoryRebound(to: integer_t.self, capacity: Int(size)) {
-            host_statistics64(machHost,
-                              HOST_VM_INFO64,
-                              $0,
-                              &size)
-        }
-
-        let data = hostInfo.move()
-        hostInfo.deallocate()
-        
-        #if DEBUG
-            if result != KERN_SUCCESS {
-                print("ERROR - \(#file):\(#function) - kern_result_t = "
-                    + "\(result)")
-            }
-        #endif
-        
         return data
     }
 }
