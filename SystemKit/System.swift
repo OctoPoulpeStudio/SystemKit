4ef71ac5067bfb655d9b12714d6d1c7334fca3f0
Ewen Riallan <121026779+EwenRiallanOcto@users.noreply.github.com>
2023-05-17 17:00:54 +0200

    CPU Speed function test

--- SystemKit/System.swift
+++ SystemKit/System.swift
@@ -133,10 +133,7 @@ public struct System {
     Get CPU usage (system, user, idle, nice). Determined by the delta between
     the current and last call. Thus, first call will always be inaccurate. This info must not be retrieved more than once per second, otherwise the returned data will be NaN
     */
-    public mutating func usageCPU() -> (system : Double,
-                                        user   : Double,
-                                        idle   : Double,
-                                        nice   : Double) {
+    public mutating func usageCPU() -> (system: Double, user: Double, idle: Double, nice: Double) {
         let load = System.hostCPULoadInfo()
         
         let userDiff = Double(load.cpu_ticks.0 - loadPrevious.cpu_ticks.0)
@@ -156,6 +153,19 @@ public struct System {
         // TODO: 2 decimal places
         // TODO: Check that total is 100%
         return (sys, user, idle, nice)
+    }
+    
+    public mutating func speedCPU() -> (system: Double, user: Double, idle: Double, nice: Double) {
+        let load = System.hostCPULoadInfo()
+        
+        let userDiff = Double(load.cpu_ticks.0 - loadPrevious.cpu_ticks.0)
+        let sysDiff  = Double(load.cpu_ticks.1 - loadPrevious.cpu_ticks.1)
+        let idleDiff = Double(load.cpu_ticks.2 - loadPrevious.cpu_ticks.2)
+        let niceDiff = Double(load.cpu_ticks.3 - loadPrevious.cpu_ticks.3)
+        
+        loadPrevious = load
+        
+        return (userDiff, sysDiff, idleDiff, niceDiff)
     }
     
     
@@ -412,13 +422,11 @@ public struct System {
     
     
     fileprivate static func hostCPULoadInfo() -> host_cpu_load_info {
-        var size     = HOST_CPU_LOAD_INFO_COUNT
+        var size = HOST_CPU_LOAD_INFO_COUNT
         let hostInfo = host_cpu_load_info_t.allocate(capacity: 1)
         
         let result = hostInfo.withMemoryRebound(to: integer_t.self, capacity: Int(size)) {
-            host_statistics(machHost, HOST_CPU_LOAD_INFO,
-                                      $0,
-                                      &size)
+            host_statistics(machHost, HOST_CPU_LOAD_INFO, $0, &size)
         }
         
         let data = hostInfo.move()
